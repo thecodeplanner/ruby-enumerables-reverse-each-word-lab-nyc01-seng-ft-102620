6c0307b729b5e2f560b8459a57a27ba0c578699d
Melissa <thecodeplanner@gmail.com>
2020-10-15 13:30:34 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_string =[]
+  array.collect do |word|
+    new_string << word.reverse
+end
+new_string.join(" ")
+end 
+
+#take in string
+#turn string into array 
+create new variale equal to empty array
+#for each word in the array, reverse word and push into new array
+#turn new array into string by joining words with " "
