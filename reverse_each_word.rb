aca058de54b8be63f6de37d621f362eda0840d57
Melissa <thecodeplanner@gmail.com>
2020-10-15 13:31:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   new_string =[]
-  array.collect do |word|
+  string.collect do |word|
     new_string << word.reverse
 end
 new_string.join(" ")
