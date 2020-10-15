d96533ba555cb500f81c831ba83f35cd43e1fdc2
Melissa <thecodeplanner@gmail.com>
2020-10-15 13:31:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,11 @@
 def reverse_each_word(string)
   array = string.split(" ")
   new_string =[]
-  string.collect do |word|
+  array.collect do |word|
     new_string << word.reverse
 end
 new_string.join(" ")
+end 
 end 
 
 #take in string
